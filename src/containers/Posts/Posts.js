ef3053e81395835ebd2fba09b8d7ff838787e18c
Aman Chermashev <bakytaman228337@gmail.com>
2019-12-19 12:59:42 +0600

    Created get function

--- src/containers/Posts/Posts.js
+++ src/containers/Posts/Posts.js
@@ -3,21 +3,45 @@ import NavBar from "../../components/NavBar/NavBar";
 import axios from 'axios';
 
 class Posts extends Component {
-    getPosts = async () => {
-        axios({
-            method: 'get',
-            url: 'https://blog-chermashev-kasymov.firebaseio.com/',
-        })
-            .then(res => {
-                console.log(res.data)
-            })
+    state = {
+      dataPosts: [],
+      posts: [],
+    };
+
+    async componentDidMount(){
+        const requests = [];
+        requests.push(axios.get('https://blog-chermashev-kasymov.firebaseio.com/post.json'));
+        const responses = await Promise.all(requests);
+        const posts = responses.map(r => r.data);
+        this.setState({dataPosts: posts});
+    }
+    callback = () => {
+
+
     };
 
     render() {
+        const data = this.state.dataPosts[0];
+        let posts = [];
+        for (let post in data){
+           post = {
+               title: data[post].title,
+               message: data[post].message,
+           };
+           posts.push(post);
+        }
         return (
             <div>
                 <NavBar/>
-                <button onClick={this.getPosts}>Hello</button>
+                <button onClick={this.callback}>Click</button>
+                {posts.map(post => {
+                    return(
+                        <div style={{border: '1px solid #ccc'}}>
+                            <p>{post.title}</p>
+                            <button>Read more</button>
+                        </div>
+                    )
+                })}
             </div>
         );
     }
